c9c31d7577be84f40bea362837bf6638ef85028a
snnik11 <48999243+snnik11@users.noreply.github.com>
2021-09-07 08:59:55 +1000

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
 import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from "enzyme";
+import  EnzymeAdapter  from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
+test('renders without error', () => {
+ const wrapper = shallow(<App/>)
+//console.log(wrapper.debug())
+//expect(wrapper.exists()).toBe(true)
+const appComponent = wrapper.find("[data-test='component-app']")
+expect(appComponent.length).toBe(1); //find only 1 element here
 });
+
+test("renders increment button",() =>{
+ const wrapper =shallow(<App/>)
+ const button = wrapper.find("[data-test='component-icrement-button]")
+ expect(button.length).toBe(1)
+})
+
+test("render counter diplay", () => {
+  const wrapper = shallow(<App/>)
+  const counterDisplay = wrapper.find("[data-test='component-counter-display']")
+  expect(counterDisplay.length).toBe(1)
+})
+
+test("counter starts at 0", ()=>{
+  const wrapper = shallow(<App/>)
+  const counterStarter = wrapper.find("[data-test='component-counter-starter']")
+  expect(counterStarter.length).toBe(1)
+})
+
+test("click button to increment counter", () =>{
+
+})
